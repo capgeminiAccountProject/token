e2f85e04bc4337d12c1461eec6d72338330e3445
Sergey <svolchenkov@gmail.com>
2019-05-29 13:26:18 -0500

    code review

--- src/main/java/com/capgemini/beans/GetTokenResponse.java
+++ src/main/java/com/capgemini/beans/GetTokenResponse.java
@@ -5,7 +5,6 @@ import com.capgemini.exceptions.Impl.TokenExceptionImpl;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Setter @Getter @NoArgsConstructor
 
---
+++ src/test/java/com/capgemini/controllers/TokenControllerTests.java
@@ -0,0 +1,17 @@
+package com.capgemini.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TokenControllerTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
